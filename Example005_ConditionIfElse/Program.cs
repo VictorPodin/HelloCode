cdf9871c21ba854697174b043bc60ee7ec2b098f
Victor Podin <podin_victor@mail.ru>
2022-11-28 23:05:34 +0300

    add Example 005 ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите име пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!!!");
+}
+else
+{
+    Console.Write("Привет, "); 
+    Console.WriteLine(username);
+}
