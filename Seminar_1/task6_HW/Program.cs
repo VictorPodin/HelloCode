d92ff914766035d3bf9bbf0a106c7b93e1a55bbd
Podin Victor <podin_victor@mail.ru>
2022-11-30 16:34:16 +0300

    third task of homework

---
+++ Seminar_1/task6_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Проверим четное ли оно:");
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Да, четное");
+}
+else
+{
+    Console.WriteLine("Нет, число нечётное");
+}
