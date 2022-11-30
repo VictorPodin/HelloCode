9ee9bbb28f56f74097d56162c8c7e4444e3fe017
Podin Victor <podin_victor@mail.ru>
2022-11-30 16:26:53 +0300

    second task of homework

---
+++ Seminar_1/task4_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите три числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Поверяем какое число максимальное:");
+if ((number1 > number2) & (number1 > number3))
+{
+    Console.WriteLine(number1);  
+}
+else if ((number2 > number3) & (number2 > number1))
+{
+    Console.WriteLine(number2);  
+}
+else if ((number3 > number2) & (number3 > number1))
+{
+    Console.WriteLine(number3); 
+}
