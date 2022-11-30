06ac523c4744f3f1d80fb80c2acca4e7e3fad0b2
Podin Victor <podin_victor@mail.ru>
2022-11-30 16:03:24 +0300

    first task of homework

---
+++ Seminar_1/task2_HW/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Поверяем какое число больше:");
+if (number1 == number2)
+{
+    Console.WriteLine("Оба числа одинаковые");  
+}
+else if (number1 > number2)
+{
+    Console.Write($"{"Первое число ("} {number1} ) больше");  
+}
+else if (number1 < number2)
+{
+    Console.Write($"{"Второе число ("} {number2} ) больше"); 
+}
