32edfa0f6404babb964c773e39211cc46ff79e16
Podin Victor <podin_victor@mail.ru>
2022-11-30 16:56:26 +0300

    fourth task of homework

---
+++ Seminar_1/task8_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int min = 2;
+if (N%2 != 0)
+{
+    N = N-1;
+}
+while (min <= N)
+{
+    Console.Write(min);
+    if(N != min)
+    {
+    Console.Write(", ");
+    }
+    min = min + 2;
+}
