81828097736a2fe3baf13d19a78c23582322dcc6
Victor Podin <podin_victor@mail.ru>
2022-12-02 22:16:31 +0300

    after seminar 2

---
+++ Seminar_2/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводит случайное число от 10 до 99
+// показывает какое число максимальное в составе этого двузначного числа
+int n = new Random().Next(10,99);
+int first = n / 10;
+int second = n % 10;
+Console.WriteLine("Выпало число: " +n);
+if(first > second)
+{
+    Console.WriteLine("Большее цифра - " +first);
+}
+else
+{
+    Console.WriteLine("Большее цифра - " +second);
+}
---
+++ Seminar_2/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Поверяем является ли одно число кратным второму:");
+if (number1%number2 == 0)
+{
+  Console.WriteLine(number1 +" кратно " + number2);  
+}
+else if (number2%number1 == 0)
+{
+  Console.WriteLine(number2 +" кратно " + number1);  
+}
+else
+{
+    if (number1 > number2)
+    {
+       Console.WriteLine("Остаток от деления " + number1 +" на " + number2 + " = " + number1%number2); 
+    }
+    else
+    {
+        Console.WriteLine("Остаток от деления " + number2 +" на " + number1 + " = " + number2%number1);
+    }
+}
