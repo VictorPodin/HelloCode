c6b72900c4a537d0c9f22d4c548a396c080d5192
Victor Podin <podin_victor@mail.ru>
2022-12-04 14:18:57 +0300

    after seminar 2 homework

---
+++ Seminar_2/task3_HW/Program.cs
@@ -0,0 +1,13 @@
+﻿// принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// добавим проверку что число трехзначное
+if (number <= 999 && number >= 100)
+{
+    Console.Write("Вторая цифра вашего числа = ");
+    Console.WriteLine(number%100/10);
+}
+else
+{
+    Console.Write("Число не трехзначное!");
+}
