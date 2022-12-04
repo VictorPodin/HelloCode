f36a7c040d5fc806f75a7097b430bac5dc89dccc
Victor Podin <podin_victor@mail.ru>
2022-12-04 19:03:39 +0300

    after seminar 2 homework3

---
+++ Seminar_2/task5_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine("Кратно 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23 одновременно!");
+}
