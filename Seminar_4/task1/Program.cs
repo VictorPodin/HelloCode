16fcc3016c01a66bff4b9291317b40b7f877fc46
Victor Podin <podin_victor@mail.ru>
2022-12-09 22:46:02 +0300

    after seminar 4

---
+++ Seminar_4/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Генерируем целочисленную переменную
+int N = new Random().Next(1, int.MaxValue);
+Console.WriteLine(N);
+// Количество цифр в числе
+int GetLenghtOfNumber(int val)
+{
+    int count = 0;
+    while(val > 0)
+    {
+        val = val / 10;
+        count++;
+    }
+    return count;
+}
+//Выделить память
+int lenght = GetLenghtOfNumber(N);
+//Заполнить цифрами
+int[] digitsofnumber = new int[lenght];
+for(int i = 0; i < lenght; i++)
+{
+    digitsofnumber[i] = N % 10;
+    N = N / 10;
+}
+for(int i = 0; i < lenght; i++)
+{
+    Console.Write(digitsofnumber[i]);
+}
