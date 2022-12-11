3d9892ff3c38f923cf816896f7d5ffb9ea3491b9
Victor Podin <podin_victor@mail.ru>
2022-12-11 18:47:56 +0300

    after seminar 4 homework2,3

---
+++ Seminar_4/task3_ HW/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int GetLenghtOfNumber(int val)  // Количество цифр в числе
+{
+    int count = 0;
+    while(val > 0)
+    {
+        val = val / 10;
+        count++;
+    }
+    return count;
+}
+int lenght = GetLenghtOfNumber(N);
+int[] digitsofnumber = new int[lenght]; 
+for(int i = 0; i < lenght; i++)   //Заполнить цифрами массив
+{
+    digitsofnumber[i] = N % 10;
+    N = N / 10;
+}
+int sum = 0;
+for(int i = lenght - 1; i >= 0; i--)     //суммируем элементы массива и заодно выведем на экран
+{
+    sum = sum + digitsofnumber[i];
+    Console.Write(digitsofnumber[i]);
+}
+Console.WriteLine(" -> " + sum);
---
+++ Seminar_4/task4_HW/Program.cs
@@ -0,0 +1,64 @@
+﻿// Назовём число «интересным» если его произведение цифр делится на их сумму.
+// Напишите программу, которая заполняет массив на 10 «интересных» случайных целых чисел от 10 до 1000.
+// (каждый эл-т массива – сгенерирован случайно)
+// [591, 532, 189, 523, 333, 546, 527, 275, 456, 264]
+int GetLenghtOfNumber(int val)  //функция чтобы брать длину выпавшего числа
+{
+    int count = 0;
+    while(val > 0)
+    {
+        val = val / 10;
+        count++;
+    }
+    return count;
+}   
+bool CheckNumberForInteresting(int Num)  //Функция проверки числа на интересность
+{
+    int sum = 0;
+    int prod = 1;
+    int lenght = GetLenghtOfNumber(Num);
+    int[] digitsofnumber = new int[lenght];    //инициируем массив под число
+    for(int i = 0; i < lenght; i++)            //заполняем его цифрами числа
+    {
+        digitsofnumber[i] = Num % 10;
+        Num = Num / 10;
+    }
+    for(int i = 0; i < lenght; i++)             //считаем сумму и произведение цифр в числе
+    {
+        sum = sum + digitsofnumber[i];
+        prod = prod * digitsofnumber[i];
+    }
+    if (prod % sum == 0 && prod != 0)          // prod != 0 исключит числа в составе которых 0, их произведение = 0...
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void PrintArray(int[] col) // Просто вывод  массива в красивом формате
+{
+    int count = col.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count-1)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+    Console.Write(col[position]+ "]");
+}
+//Сделали нужные функции, теперть к задаче
+int[] array = new int[10];
+int i = 0;
+while (i < 10)
+{
+    int number = new Random().Next(10, 1001);  //генерим число
+    if (CheckNumberForInteresting(number) == true)
+    {
+        array[i] = number;
+        i++;
+    }
+}
+PrintArray(array);
