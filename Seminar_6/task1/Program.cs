0f923ca8c88cd1b8c35ca9602bff3be429353b15
Podin Victor <podin_victor@mail.ru>
2022-12-19 16:06:12 +0300

    seminar 6

---
+++ Seminar_6/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// рекурсия факториал
+int get_factorial(int x)
+{
+    Console.WriteLine(x);
+    if (x == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        int res = get_factorial(x - 1);
+        Console.WriteLine(res);
+        return x * res;
+    }
+}
+
+
+Console.WriteLine(get_factorial(5));
