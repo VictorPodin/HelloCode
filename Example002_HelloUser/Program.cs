59b607e1e7d47ad96ae804d3995d51cde15e60cc
Victor Podin <podin_victor@mail.ru>
2022-12-21 23:29:13 +0300

    seminar 7 hw 3

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя");
+string username = Convert.ToString(Console.ReadLine());
+Console.Write("Привет, ");
+Console.Write(username);
---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1, 10); // это случайные числа 1,2,3.....9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
