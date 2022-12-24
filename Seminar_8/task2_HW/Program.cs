9b62a3a817d7e8d337e64fed722c64d0082da97e
Victor Podin <podin_victor@mail.ru>
2022-12-24 23:36:19 +0300

    after seminar 8 homework 1,2

---
+++ Seminar_8/task2_HW/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 1: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+//
+//Сделаем вводимыми число строк и столбцов и заполним массив рандомами с одной цифрой, (0;9)
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+void FillArrayRandomNumbers(int[,] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void SortArrayMaxToMinInRow(int[,] arr)
+{
+    int tmp;
+    bool flag; // этой переменной делаю пометку в массиве. Если при проходе ничего менять местами не пришлось - отсортирован.
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        flag = false; // для входа в цикл сортировки
+        while (flag == false)  //мне просто интересно, не лучше ли так чем через for c
+        {
+          flag = true; // если не придется ничего сортировать значение останется
+          for (int j = 0; j < arr.GetLength(1) - 1; j++)
+          {
+              if (arr[i, j + 1] > arr[i, j])
+                {
+                    tmp = arr[i, j];
+                    arr[i, j] = arr[i, j + 1];
+                    arr[i, j + 1] = tmp;
+                    flag = false; 
+                }
+           } 
+        }
+    }
+}
+FillArrayRandomNumbers(array);
+PrintArray(array);
+Console.WriteLine();
+SortArrayMaxToMinInRow(array);
+PrintArray(array);
---
+++ Seminar_8/task3_HW/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+//
+// Сделаем вводимыми размерность массива массив рандомами с одной цифрой, (0;9)
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+void FillArrayRandomNumbers(int[,] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+Console.WriteLine();
+int[] GetArraySumRows(int[,] arr)
+{
+    int[] sumArr = new int[arr.GetLength(0)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sumArr[i] += arr[i, j];
+        }
+    }
+    return sumArr;
+}
+int[] newArr = GetArraySumRows(array);
+// Для проверки можно раскомментировать ниже
+// for (int i = 0; i < newArr.Length; i++)
+// {
+//     Console.WriteLine(newArr[i]);
+// }
+int GetIndexOfMin(int[] arr)
+{
+    int min = arr[0];
+    int indexMin = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            indexMin = i;
+        }
+    }
+    return indexMin;
+}
+Console.WriteLine();
+int stringMin = GetIndexOfMin(newArr) + 1;
+Console.WriteLine(stringMin + " строка");
