0e2278f8b26d9dd1f8c25c8a627827d76bf5880f
Victor Podin <podin_victor@mail.ru>
2022-12-25 17:20:01 +0300

    after seminar 8 homework 3,4

---
+++ Seminar_8/task4_HW/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задача 3: Задайте две квадратные матрицы одинакового размера. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица:
+// 18 20
+// 15 18
+//
+// Сделаем вводимыми размерность КВАДРАТНОГО массива рандомами с одной цифрой, (0;9)
+Console.Write("Введите размерность двух квадратных матриц: ");
+int dim = Convert.ToInt32(Console.ReadLine());
+int[,] matrix1 = new int[dim, dim];
+int[,] matrix2 = new int[dim, dim];
+void FillArrayRandomNumbers(int[,] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void PrintTwoQuadMatrix(int[,] arr1, int[,] arr2)
+{
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr1.GetLength(1); j++)
+        {
+            Console.Write(arr1[i, j] + " ");
+        }
+        Console.Write("| ");
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+            Console.Write(arr2[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArrayRandomNumbers(matrix1);
+// PrintArray(matrix1);
+// Console.WriteLine();
+FillArrayRandomNumbers(matrix2);
+// PrintArray(matrix2);
+// Console.WriteLine();
+PrintTwoQuadMatrix(matrix1, matrix2);
+int[,] MultiplyTwoMatrix(int[,] arr1, int[,] arr2)
+{
+    int[,] resMatrix = new int[arr1.GetLength(0), arr2.GetLength(1)];
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr2.GetLength(0); k++)
+            {
+                resMatrix[i, j] += arr1[i, k] * arr2[k, j];
+            }
+        }
+    }
+    return resMatrix;
+}
+int[,] MultiplyMatrix = MultiplyTwoMatrix(matrix1, matrix2);
+Console.WriteLine("Результат перемножения матриц:");
+PrintArray(MultiplyMatrix);
---
+++ Seminar_8/task5_HW/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 4*. Сформируйте двухмерный массив из неповторяющихся случайных двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив. 
+// Обратите внимание, что максимальный размер такого массива ограничен. 
+// Проверку эл-та на присутствие в массиве можно вынести в отдельную функцию.
+// Например,
+// 11 22 78
+// 12 47 96
+// 25 87 88
+//
+// Сделаем вводимыми размерность КВАДРАТНОГО массива рандомами с одной цифрой, (0;9)
+//Сделаем вводимыми число строк и столбцов и заполним массив рандомами с одной цифрой, (0;9)
+void FillArrayRandomUniqNumbers(int[,] arr)
+{
+    Random random = new Random();
+    int number;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            number = random.Next(10, 100);
+            while (CheckRepeatElem(arr, number) == true) 
+            {
+                 number = random.Next(10, 100);
+            }
+            arr[i, j] = number;
+        }
+    }
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+bool CheckRepeatElem(int[,] arr, int num)
+{
+    bool flag = false;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == num)
+            {
+                flag = true;
+            }
+        }
+    }
+    return flag;
+}
+Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+if(rows*columns < 90)
+{
+    int[,] array = new int[rows, columns];
+    FillArrayRandomUniqNumbers(array);
+    PrintArray(array);
+}
+else
+{
+    Console.WriteLine("В такой размерности не получится заполнить массив неповторяющимися двузначными числами");
+}
