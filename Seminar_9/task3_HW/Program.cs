1b6f5ee78e7759c210b58a2be8fd8f44f4bcd790
Victor Podin <podin_victor@mail.ru>
2022-12-26 23:37:17 +0300

    after seminar 9 homework 3,4

--- Seminar_9/task3_HW/Program.cs
+++ Seminar_9/task3_HW/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9 (?)
+// m = 3, n = 2 -> A(m,n) = 29 (?)
+Console.WriteLine("Введите значение m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение n:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("m = " + m + "; n = " + n + "  → A(m,n) = ");
+int CalculateAkkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if ((m > 0) && (n == 0))
+    {
+        return CalculateAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return CalculateAkkerman(m - 1, CalculateAkkerman(m, n - 1));
+    }
+}
+Console.WriteLine(CalculateAkkerman(m, n));
---
+++ Seminar_9/task4_HW/Program.cs
@@ -0,0 +1,90 @@
+﻿// Задача 4*. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+//
+Console.WriteLine("Введите количество строк/столбцов квадратного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+//  1-право, 2-вниз, 3-влево, 4-вверх
+int[,] arraySpiral = new int[n, n];
+void PrintStringArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] >= 10)
+            {
+                Console.Write(Convert.ToString(arr[i, j]) + " ");
+            }
+            else
+            {
+                Console.Write("0" + Convert.ToString(arr[i, j]) + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+void FillArraySpiral(int[,] arr)
+{
+    int bottom = arr.GetLength(0) - 1;
+    int right = arr.GetLength(1) - 1;
+    int left = 0;
+    int top = 0;
+    int colElem = arr.GetLength(0) * arr.GetLength(1);
+    int countElem = 0;
+    int startElem = 1;
+    int direction = 1;  //  1-право, 2-вниз, 3-влево, 4-вверх
+    while (countElem < colElem)
+    {
+        if (direction == 1)
+        {
+            for (int j = left, i = top; j <= right; j++)
+            {
+                arr[i, j] = startElem;
+                startElem++;
+                countElem++;
+            }
+            top++;
+            direction++;
+        }
+        if (direction == 2)
+        {
+            for (int i = top, j = right; i <= bottom; i++)
+            {
+                arr[i, j] = startElem;
+                startElem++;
+                countElem++;
+            }
+            right--;
+            direction++;
+        }
+        if (direction == 3)
+        {
+            for (int j = right, i = bottom; j >= left; j--)
+            {
+                arr[i, j] = startElem;
+                startElem++;
+                countElem++;
+            }
+
+            bottom--;
+            direction++;
+        }
+        if (direction == 4)
+        {
+            for (int i = bottom, j = left; i >= top; i--)
+            {
+                arr[i, j] = startElem;
+                startElem++;
+                countElem++;
+            }
+            left++;
+            direction = 1;
+        }
+    }
+}
+FillArraySpiral(arraySpiral);
+PrintStringArray(arraySpiral);
