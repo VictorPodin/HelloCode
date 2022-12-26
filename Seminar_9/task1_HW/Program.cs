71819516d9fb9c3afd6390c02dd064748d0f9560
Victor Podin <podin_victor@mail.ru>
2022-12-26 18:04:18 +0300

    after seminar 9 homework 1,2

---
+++ Seminar_9/task1_HW/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+Console.WriteLine("Введите значение N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("N = " + N + "  → ＂");
+void PrintNumsFromNto1(int n)
+{
+    if (n == 1)
+    {
+        Console.Write(n);
+        return;
+    }
+    else
+    {
+        Console.Write(n + ", ");
+        PrintNumsFromNto1(n - 1);
+    }
+}
+PrintNumsFromNto1(N);
+Console.Write("＂");
---
+++ Seminar_9/task2_HW/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 2: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumNumsFromMtoN(int m, int n)
+{
+    if (n == m) return m;
+    if (m < n) return m + SumNumsFromMtoN(m + 1, n);
+    else return m + SumNumsFromMtoN(m - 1, n);
+}
+Console.WriteLine("Введите значение M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("M = " + M + "; N = " + N + "  → ");
+if (M != N) 
+{
+    int mnSum = SumNumsFromMtoN(M, N);
+    Console.Write(mnSum);
+}
+else { Console.WriteLine("Числа одинаковые, а надо чтоб был промежуток");}
